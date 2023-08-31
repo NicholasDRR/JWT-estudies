2b033081561df47a9c10d9cc28b5fa71be8d2bac
NicholasDRR <nicholasreis00@gmail.com>
2023-08-31 01:06:21 -0300

    feat(login) creating route to login user

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter
-from fastapi import Depends
+from fastapi import APIRouter, Depends, status
 from fastapi.responses import JSONResponse
+from fastapi.security import OAuth2PasswordRequestForm
 
 from app.schemas import User
 from app.db.depends import get_db_session
@@ -11,10 +11,25 @@ router = APIRouter(
     prefix="/user"
 )
 
-@router.post("/")
+@router.post("/register")
 def create_user(user: User, db_session: Session = Depends(get_db_session)):
     
     uc = UserUseCases(db_session)
     uc.user_register(user)
     
     return JSONResponse(content={"msg": "User successfully created!"})
+
+
+
+@router.post("/login")
+def login_user(request_form_user: OAuth2PasswordRequestForm = Depends(), db_session: Session = Depends(get_db_session)):
+    
+    uc = UserUseCases(db_session)
+    user = User(
+        username=request_form_user.username,
+        password=request_form_user.password
+    )
+    
+    auth_data = uc.user_login(user)
+    return JSONResponse(content=auth_data, status_code=status.HTTP_200_OK)
+    
